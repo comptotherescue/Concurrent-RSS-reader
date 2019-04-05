b5ae4211ed2c01b8de7532ae6b2559df902ec3df
comptotherescue <a.kulkarni110@yahoo.in>
2019-04-05 17:25:14 -0400

    AK - Insert class completed

--- Cp Project/src/cp/project/InsertIntoDB.java
+++ Cp Project/src/cp/project/InsertIntoDB.java
@@ -1,13 +1,22 @@
 package cp.project;
 
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.util.Set;
+
 import org.bson.Document;
 
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.DBCollectionCountOptions;
 
 public class InsertIntoDB {
 	MongoClient client = null;
@@ -15,6 +24,15 @@ public class InsertIntoDB {
 		DBclass db = new DBclass();
 		client = db.getInstance();
 		MongoDatabase Db = client.getDatabase("RSSFeed");
+		MongoIterable<String> collections = Db.listCollectionNames();
+		for(String k : collections) {
+			System.out.println(k);
+		}
+		Gson gson = new Gson();
+		String json = gson.toJson(RSSobj);              
+		MongoCollection<Document> collection = Db.getCollection("ESPN");  
 		
+		Document doc = new Document("GUID",RSSobj.getGuid()).append("JSON", json);
+		collection.insertOne(doc); 
 	}
 }
