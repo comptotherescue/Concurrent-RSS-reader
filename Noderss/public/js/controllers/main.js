85968d788bc28c71fb9e1541f9e09ac68d44ccb1
comptotherescue <a.kulkarni110@yahoo.in>
2019-04-18 06:55:17 -0400

    Front end in AngularJS and node.js AK

---
+++ Noderss/public/js/controllers/main.js
@@ -0,0 +1,66 @@
+angular.module('todoController', [])
+
+	// inject the Todo service factory into our controller
+	.controller('mainController', ['$scope','$http','Todos', function($scope, $http, Todos) {
+		$scope.formData = {};
+		$scope.loading = true;
+
+		// GET =====================================================================
+		// when landing on the page, get all todos and show them
+		// use the service to get all the todos
+		Todos.get()
+			.success(function(data) {
+				$scope.todos = data;
+				console.log(data);
+				$scope.loading = false;
+			});
+
+		// CREATE ==================================================================
+		// when submitting the add form, send the text to the node API
+		$scope.createTodo = function() {
+
+			// validate the formData to make sure that something is there
+			// if form is empty, nothing will happen
+			if ($scope.formData.text != undefined) {
+				$scope.loading = true;
+
+				// call the create function from our service (returns a promise object)
+				Todos.create($scope.formData)
+
+					// if successful creation, call our get function to get all the new todos
+					.success(function(data) {
+						$scope.loading = false;
+						$scope.formData = {}; // clear the form so our user is ready to enter another
+						$scope.todos = data; // assign our new list of todos
+					});
+			}
+		};
+
+		
+	
+
+    	$scope.onClick = function(x) {
+    		console.log(x.Name);
+    		var url2= "http://localhost:3000/api/getCollection/" +x.Name;
+    		console.log(url2);
+    		$http.get(url2)
+  				 .then(function(response) {
+  				 console.log(response.data);
+    		$scope.myWelcome = response.data;
+  				});
+
+    	}
+
+		// DELETE ==================================================================
+		// delete a todo after checking it
+		$scope.deleteTodo = function(id) {
+			$scope.loading = true;
+
+			Todos.delete(id)
+				// if successful creation, call our get function to get all the new todos
+				.success(function(data) {
+					$scope.loading = false;
+					$scope.todos = data; // assign our new list of todos
+				});
+		};
+	}]);
