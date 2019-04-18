35a10c04f56b7a197759e579b5c2cf7dae1101f4
Rohit Banka <banka.rohit1809@gmail.com>
2019-04-18 10:06:56 -0400

    Constants Renamed

--- Cp Project/src/cp/project/Constants.java
+++ Cp Project/src/cp/project/Constants.java
@@ -24,9 +24,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
 		subscriptions.add(new Pair<String,String>("https://hacked.com/feed","Hacked"));
-		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","techrepublic"));
+		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","Techrepublic"));
 		subscriptions.add(new Pair<String,String>("https://themerkle.com/feed/","TheMerkle"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","NAIC"));
 		subscriptions.add(new Pair<String,String>("https://rss.dailyfx.com/feeds/all","DailyFx"));
 		}
 	
@@ -37,9 +37,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
 		subscriptions.add(new Pair<String,String>("https://hacked.com/feed","Hacked"));
-		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","techrepublic"));
+		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","Techrepublic"));
 		subscriptions.add(new Pair<String,String>("https://themerkle.com/feed/","TheMerkle"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","NAIC"));
 		subscriptions.add(new Pair<String,String>("https://rss.dailyfx.com/feeds/all","DailyFx"));
 		subscriptions.add(new Pair<String,String>("https://www.which.co.uk/news/feed/","Which"));
 		subscriptions.add(new Pair<String,String>("https://www.technologyreview.com/stories.rss","TechnologyReview"));
@@ -48,7 +48,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://wccftech.com/feed/","WccFTech"));
 		subscriptions.add(new Pair<String,String>("https://boingboing.net/feed","BoingBoing"));
 		subscriptions.add(new Pair<String,String>("https://www.prnewswire.com/rss/all-news-releases-from-PR-newswire-news.rss","PRNNewsWire"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","naic2"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","NAICII"));
 		subscriptions.add(new Pair<String,String>("https://www.ccn.com/feed/","CNN"));
 		subscriptions.add(new Pair<String,String>("http://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml","NZHerald"));
 		}
@@ -60,9 +60,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
 		subscriptions.add(new Pair<String,String>("https://hacked.com/feed","Hacked"));
-		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","techrepublic"));
+		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","Techrepublic"));
 		subscriptions.add(new Pair<String,String>("https://themerkle.com/feed/","TheMerkle"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","NAIC"));
 		subscriptions.add(new Pair<String,String>("https://rss.dailyfx.com/feeds/all","DailyFx"));
 		subscriptions.add(new Pair<String,String>("https://www.which.co.uk/news/feed/","Which"));
 		subscriptions.add(new Pair<String,String>("https://www.technologyreview.com/stories.rss","TechnologyReview"));
@@ -71,7 +71,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://wccftech.com/feed/","WccFTech"));
 		subscriptions.add(new Pair<String,String>("https://boingboing.net/feed","BoingBoing"));
 		subscriptions.add(new Pair<String,String>("https://www.prnewswire.com/rss/all-news-releases-from-PR-newswire-news.rss","PRNNewsWire"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","naic2"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","NAICII"));
 		subscriptions.add(new Pair<String,String>("https://www.ccn.com/feed/","CNN"));
 		subscriptions.add(new Pair<String,String>("http://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml","NZHerald"));
 		subscriptions.add(new Pair<String,String>("https://cointelegraph.com/feed","CoinTelegraph"));
@@ -82,7 +82,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("http://fortune.com/feed/","Fortune"));
 		subscriptions.add(new Pair<String,String>("https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en","Googlefeed"));
 		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/educationnews.rss","Nasa"));
-		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/breaking_news.rss","Nasa2"));
+		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/breaking_news.rss","NasaII"));
 		subscriptions.add(new Pair<String,String>("https://blogs.nasa.gov/stationreport/feed/","NasaISS"));
 		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss","NasaKeplerMission"));
 		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/chandra_images.rss","NasaChandraMission"));
@@ -90,7 +90,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://rss.weatherzone.com.au/?u=12994-1285&lt=aploc&lc=12495&obs=1&fc=1&warn=1","WeatherZone"));
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
-		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss","NasaKeplerMission"));
+		//subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss","NasaKeplerMission"));
 	}
 	
 	public static void init_huge() {
@@ -100,9 +100,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
 		subscriptions.add(new Pair<String,String>("https://hacked.com/feed","Hacked"));
-		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","techrepublic"));
+		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","Techrepublic"));
 		subscriptions.add(new Pair<String,String>("https://themerkle.com/feed/","TheMerkle"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
+		//subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
 		subscriptions.add(new Pair<String,String>("https://rss.dailyfx.com/feeds/all","DailyFx"));
 		subscriptions.add(new Pair<String,String>("https://www.which.co.uk/news/feed/","Which"));
 		subscriptions.add(new Pair<String,String>("https://www.technologyreview.com/stories.rss","TechnologyReview"));
@@ -111,7 +111,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://wccftech.com/feed/","WccFTech"));
 		subscriptions.add(new Pair<String,String>("https://boingboing.net/feed","BoingBoing"));
 		subscriptions.add(new Pair<String,String>("https://www.prnewswire.com/rss/all-news-releases-from-PR-newswire-news.rss","PRNNewsWire"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","naic2"));
+//		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","naic2"));
 		subscriptions.add(new Pair<String,String>("https://www.ccn.com/feed/","CNN"));
 		subscriptions.add(new Pair<String,String>("http://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml","NZHerald"));
 		subscriptions.add(new Pair<String,String>("https://cointelegraph.com/feed","CoinTelegraph"));
@@ -131,9 +131,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://www.npr.org/rss/podcast.php?id=510298","NPR"));
 		subscriptions.add(new Pair<String,String>("https://business.financialpost.com/feed/","FinancialPost"));
 		subscriptions.add(new Pair<String,String>("https://hacked.com/feed","Hacked"));
-		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","techrepublic"));
+		subscriptions.add(new Pair<String,String>("https://www.techrepublic.com/rssfeeds/articles/","Techrepublic"));
 		subscriptions.add(new Pair<String,String>("https://themerkle.com/feed/","TheMerkle"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","naic"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/press.xml","NAIC"));
 		subscriptions.add(new Pair<String,String>("https://rss.dailyfx.com/feeds/all","DailyFx"));
 		subscriptions.add(new Pair<String,String>("https://www.which.co.uk/news/feed/","Which"));
 		subscriptions.add(new Pair<String,String>("https://www.technologyreview.com/stories.rss","TechnologyReview"));
@@ -142,7 +142,7 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("https://wccftech.com/feed/","WccFTech"));
 		subscriptions.add(new Pair<String,String>("https://boingboing.net/feed","BoingBoing"));
 		subscriptions.add(new Pair<String,String>("https://www.prnewswire.com/rss/all-news-releases-from-PR-newswire-news.rss","PRNNewsWire"));
-		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","naic2"));
+		subscriptions.add(new Pair<String,String>("https://www.naic.org/newswire.xml","NAIC-II"));
 		subscriptions.add(new Pair<String,String>("https://www.ccn.com/feed/","CNN"));
 		subscriptions.add(new Pair<String,String>("http://rss.nzherald.co.nz/rss/xml/nzhtsrsscid_000000698.xml","NZHerald"));
 		subscriptions.add(new Pair<String,String>("https://cointelegraph.com/feed","CoinTelegraph"));
@@ -153,9 +153,9 @@ public class Constants {
 		subscriptions.add(new Pair<String,String>("http://fortune.com/feed/","Fortune"));
 		subscriptions.add(new Pair<String,String>("https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US:en","Googlefeed"));
 		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/educationnews.rss","Nasa"));
-		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/breaking_news.rss","Nasa2"));
+		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/breaking_news.rss","NasaII"));
 		subscriptions.add(new Pair<String,String>("https://blogs.nasa.gov/stationreport/feed/","NasaISS"));
-		subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss","NasaKeplerMission"));
+		//subscriptions.add(new Pair<String,String>("https://www.nasa.gov/rss/dyn/mission_pages/kepler/news/kepler-newsandfeatures-RSS.rss","NasaKeplerMission"));
 	}
 	
 	public static List<Pair<String,String>> getSubscriptions() {
